81de679bfaf8dfeeb9bccead2051847c118bbf70
Jerome Otieno <sakajerome11@gmail.com>
2023-07-06 01:24:04 +0300

    Initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+kamala_khan = Hero.create(name: "Kamala Khan", super_name: "Ms. Marvel")
+doreen_green = Hero.create(name: "Doreen Green", super_name: "Squirrel Girl")
+gwen_stacy = Hero.create(name: "Gwen Stacy", super_name: "Spider-Gwen")
+
+super_strength = Power.create(name: "super strength", description: "gives the wielder super-human strengths")
+flight = Power.create(name: "flight", description: "gives the wielder the ability to fly through the skies at supersonic speed")
+
+kamala_khan.powers << super_strength
+kamala_khan.powers << flight
